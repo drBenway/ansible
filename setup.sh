983150ac875769babfad3841e4c037533d7f92c1
nonoman <nonoman@gmail.com>
2020-09-16 17:34:58 +0200

    adding logo to display in terminal

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 sudo apt-get update
 
 // install dependencies
-sudo apt-get install -y  python3-pip  git ansible
+sudo apt-get install -y  python3-pip  git ansible jp2a
 
 // add the roles to galaxy so the playbooks work
 ansible-galaxy install -r requirements.yml --force
 
 //docker
+jp2a https://github.com/drBenway/ansible-docker/docker_logo.jpg --color --full
 ansible-pull -i localhost -U https://github.com/drBenway/ansible-docker playbook.yml
 
+//intellij
+jp2a https://github.com/drBenway/ansible-intellij/intellij_logo.jpg --color --full
+ansible-pull -i localhost -U https://github.com/drBenway/ansible-intellij playbook.yml
+
 // sublime
+jp2a https://github.com/drBenway/ansible-sublime/sublime_logo.jpg --color --full
 ansible-pull -i localhost -U https://github.com/drBenway/ansible-sublime playbook.yml
 
 // visual studio
+jp2a https://github.com/drBenway/ansible-vs/visualstudio_logo.jpg --color --full
 ansible-pull -i localhost -U https://github.com/drBenway/ansible-vs playbook.yml
